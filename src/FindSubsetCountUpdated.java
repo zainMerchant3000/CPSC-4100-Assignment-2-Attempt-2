5598b0b4aeb63e30276c48daf2f38340a2a24522
Zain-Abbas <zain786@outlook.com>
2025-02-22 13:48:22 -0800

    Working on point compression in updated version of SubsetCount

--- src/FindSubsetCountUpdated.java
+++ src/FindSubsetCountUpdated.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class FindSubsetCountUpdated {
     static int[][] psum;
@@ -24,11 +25,13 @@ public class FindSubsetCountUpdated {
 
     static int solve(int[][] points) {
         N = points.length;
+        // create preSum matrix
         psum = new int[N + 1][N + 1];
         ss = new int[N];
         // O(N^2) time
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
+                //
                 if (points[i - 1][j - 1] > 0) ss[i - 1] = j - 1;
                 psum[i][j] = points[i - 1][j - 1] - psum[i - 1][j] - psum[i][j - 1] + psum[i - 1][j - 1];
             }
@@ -47,40 +50,91 @@ public class FindSubsetCountUpdated {
     // returns: [rank] -> [value in ps]
     static HashMap<Integer, Integer> compressx(int[][] ps, int coord) {
         for (int i = 0; i < N; i++)
+            // ps[i][0] -> retrieving x coordinate of each point
+            // ps[i][1] -> retrieving y coordinate of each point
+            // ex) ps[0][0] = 0, ps[1][0] = 2 ... etc)
             qs[i] = ps[i][coord];
+
+            System.out.println("qs[i]: " + Arrays.toString(qs));
+        // sort the x and y coordinates
         Arrays.sort(qs);
+        System.out.println(" Sorted qs[i]: " + Arrays.toString(qs));
+        // create mapping of coordinates
+        // ex) m = {0: 0, 1:1}
         var m = new HashMap<Integer, Integer>();
-        for (int i = 0; i < N; i++)
-            m.put(qs[i], i);
+        for (int i = 0; i < N; i++) {
+            // (populate map with sorted x and y coordinates)
+            Integer previousValue =  m.put(qs[i], i);
+            System.out.println("Inserted (" + qs[i] + ", " + i + "), previous value: " + previousValue);
+        }
         return m;
     }
 
     // .[0] -> the scalar; .[1] -> order as found in original order
     static void compress(int[][] points) {
         qs = new int[N];
+        // compress both x and y coordinates
         var xs = compressx(points, 0);
         var ys = compressx(points, 1);
+        // Print xs
+        System.out.println("xs:");
+        for (Map.Entry<Integer, Integer> entry : xs.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+        // Print ys
+        System.out.println("ys:");
+        for (Map.Entry<Integer, Integer> entry : ys.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
         for (int i = 0; i < N; i++) {
-            points[i][0] = xs.get(points[i][1]);
-            points[i][1] = ys.get(points[i][0]);
+            // iterate and update each point to compressed x-y values
+            Integer xValue = xs.get(points[i][0]);
+            Integer yValue = ys.get(points[i][1]);
+            System.out.println("xValue: " + xValue + ", yValue: " + yValue);
+
+
+           // points[i][0] = xs.get(points[i][1]);
+          //  points[i][1] = ys.get(points[i][0]);
         }
     }
 
     public static void main(String[] args) throws IOException {
-        var bufread = new BufferedReader(new FileReader(args[0]));
+        var BufferedReader = new BufferedReader(new FileReader(args[0]));
+        // create StringBuilder object to build string
         var build = new StringBuilder();
+        // store each line in file
         var line = "";
-        while ((line = bufread.readLine()) != null) {
+        //reading each line from file
+        while ((line = BufferedReader.readLine()) != null) {
+            //appending lines
             build.append(line).append(" ");
         }
-        bufread.close();
+        BufferedReader.close();
+        //storing input as string of tokens
+        // ex ["5", "0", "3", "2","2","1","1","3","0","4","4"]
         var tokens = build.toString().split(" ");
+        System.out.println(tokens[0]);
+        // tokens[0] = "5"
+        // parsing token (line) as Integer
+        // ex) 5 -> tells us number of points
         N = Integer.parseInt(tokens[0]);
+        // create 2D array Points
         var points = new int[N][N];
+        // k = 1 -> start at line 1 to process each pair of points
+        // Integer.parseInt(tokens[k]) -> provides us value of points in given line
         for (int i = 0, k = 1; i < N; i++) {
             for (int j = 0; j < 2; j++, k++) {
+                // points[0][0] = 0
+                // points[0][1] = 3
                 points[i][j] = Integer.parseInt(tokens[k]);
             }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(points[i][j] + " ");
+            }
+            System.out.println();
         }
         compress(points);
         System.out.println(solve(points));
