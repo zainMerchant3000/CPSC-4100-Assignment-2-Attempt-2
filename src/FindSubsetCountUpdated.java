87edffb06adde5277078eb4d3fc09d45f11879f6
Zain-Abbas <zain786@outlook.com>
2025-02-22 16:14:01 -0800

    fixed point compression (not sure if optimal)

--- src/FindSubsetCountUpdated.java
+++ src/FindSubsetCountUpdated.java
@@ -23,20 +23,34 @@ public class FindSubsetCountUpdated {
         return psum[x2 + 1][y2 + 1] - psum[x2 + 1][y1] - psum[x1][y2 + 1] + psum[x1][y1];
     }
 
-    static int solve(int[][] points) {
-        N = points.length;
-        // create preSum matrix
+    static int solve(int[][] markedMatrix) {
+        N = markedMatrix.length;
+        // create preSum points
         psum = new int[N + 1][N + 1];
+        //
         ss = new int[N];
         // O(N^2) time
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                //
-                if (points[i - 1][j - 1] > 0) ss[i - 1] = j - 1;
-                psum[i][j] = points[i - 1][j - 1] - psum[i - 1][j] - psum[i][j - 1] + psum[i - 1][j - 1];
+                // if (points[i - 1][j - 1] > 0) ->
+                if (markedMatrix[i - 1][j - 1] > 0) {
+                    ss[i - 1] = j - 1;
+                    psum[i][j] = markedMatrix[i - 1][j - 1] + psum[i - 1][j] + psum[i][j - 1] - psum[i - 1][j - 1];
+                }
+                psum[i][j] = markedMatrix[i - 1][j - 1] + psum[i - 1][j] + psum[i][j - 1] - psum[i - 1][j - 1];
             }
         }
+        /*
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                System.out.print(psum[i][j] + " ");
+            }
+            System.out.println();
+        }
+        */
+        // Base case: #of subsets = N (number of singleton sets) + 1 empty set
         int sum = 1 + N;
+        //
         for (int pr = 0; pr < N-1; pr++) {
             for (int qr = pr + 1; qr < N; qr++) {
                 int pc = Math.min(ss[pr], ss[qr]);
@@ -55,17 +69,17 @@ public class FindSubsetCountUpdated {
             // ex) ps[0][0] = 0, ps[1][0] = 2 ... etc)
             qs[i] = ps[i][coord];
 
-            System.out.println("qs[i]: " + Arrays.toString(qs));
+           // System.out.println("qs[i]: " + Arrays.toString(qs));
         // sort the x and y coordinates
         Arrays.sort(qs);
-        System.out.println(" Sorted qs[i]: " + Arrays.toString(qs));
+       // System.out.println(" Sorted qs[i]: " + Arrays.toString(qs));
         // create mapping of coordinates
         // ex) m = {0: 0, 1:1}
         var m = new HashMap<Integer, Integer>();
         for (int i = 0; i < N; i++) {
             // (populate map with sorted x and y coordinates)
             Integer previousValue =  m.put(qs[i], i);
-            System.out.println("Inserted (" + qs[i] + ", " + i + "), previous value: " + previousValue);
+           // System.out.println("Inserted (" + qs[i] + ", " + i + "), previous value: " + previousValue);
         }
         return m;
     }
@@ -77,29 +91,39 @@ public class FindSubsetCountUpdated {
         var xs = compressx(points, 0);
         var ys = compressx(points, 1);
         // Print xs
+        /*
         System.out.println("xs:");
         for (Map.Entry<Integer, Integer> entry : xs.entrySet()) {
             System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
         }
+        */
+
 
         // Print ys
+        /*
         System.out.println("ys:");
         for (Map.Entry<Integer, Integer> entry : ys.entrySet()) {
             System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
         }
+        */
+
         for (int i = 0; i < N; i++) {
             // iterate and update each point to compressed x-y values
+            // i = 0
+            // xs.get(points[i][0]) -> look up x-coordinate in map to find value
+            // xs.get(points[0][i]) -> look up y-coordinate in map to find value
             Integer xValue = xs.get(points[i][0]);
             Integer yValue = ys.get(points[i][1]);
-            System.out.println("xValue: " + xValue + ", yValue: " + yValue);
-
-
-           // points[i][0] = xs.get(points[i][1]);
-          //  points[i][1] = ys.get(points[i][0]);
+           // System.out.println("xValue: " + xValue + ", yValue: " + yValue);
+            points[i][0] = xValue;
+            points[i][1] = yValue;
         }
+
     }
 
     public static void main(String[] args) throws IOException {
+        // Start Measuring Time:
+        long startTime = System.currentTimeMillis();
         var BufferedReader = new BufferedReader(new FileReader(args[0]));
         // create StringBuilder object to build string
         var build = new StringBuilder();
@@ -114,12 +138,12 @@ public class FindSubsetCountUpdated {
         //storing input as string of tokens
         // ex ["5", "0", "3", "2","2","1","1","3","0","4","4"]
         var tokens = build.toString().split(" ");
-        System.out.println(tokens[0]);
+       // System.out.println(tokens[0]);
         // tokens[0] = "5"
         // parsing token (line) as Integer
         // ex) 5 -> tells us number of points
         N = Integer.parseInt(tokens[0]);
-        // create 2D array Points
+        // create 2D array points
         var points = new int[N][N];
         // k = 1 -> start at line 1 to process each pair of points
         // Integer.parseInt(tokens[k]) -> provides us value of points in given line
@@ -130,13 +154,70 @@ public class FindSubsetCountUpdated {
                 points[i][j] = Integer.parseInt(tokens[k]);
             }
         }
+        /*
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 System.out.print(points[i][j] + " ");
             }
             System.out.println();
         }
+
+         */
+        // compress the points
         compress(points);
-        System.out.println(solve(points));
+        /*
+        for (int row = 0; row < N; row++) {
+            System.out.println(" points[row][0] " + points[row][0] + " " + points[row][1]);
+        }
+        */
+        /*
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(points[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+         */
+
+        // create the matrix:
+        int[][] markedMatrix = createMarkedMatrix(points);
+        // Print the marked matrix
+        System.out.println("marked matrix");
+        //printMatrix(markedMatrix);
+        int result = solve(markedMatrix);
+       // System.out.println(solve(markedMatrix));
+        // End measuring time
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        // Output the time taken
+        System.out.println("Time taken: " + totalTime + " milliseconds");
     }
+
+     static int[][] createMarkedMatrix(int[][] points) {
+         // Initialize the matrix with zeros
+         int[][] matrix = new int[N][N];
+         for (int i = 0; i < N; i++) {
+             Arrays.fill(matrix[i], 0);
+         }
+
+         // Mark the points in the matrix
+         for (int i = 0; i < points.length; i++) {
+             int x = points[i][0];
+             int y = points[i][1];
+             matrix[x][y] = 1;
+         }
+         return matrix;
+     }
+
+    static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+               // System.out.print(matrix[i][j] + " ");
+            }
+           // System.out.println();
+        }
+    }
+
+
 }
